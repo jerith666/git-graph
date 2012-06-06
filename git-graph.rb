398e7107a655f9d3526b9c2c6b79b030bb11a372
Matt McHenry <github@matt.mchenryfamily.org>
2012-06-06 17:54:58 -0400

    first cut at plotting with boring nodes elided

--- git-graph.rb
+++ git-graph.rb
@@ -15,57 +15,83 @@ def find_children (commit, children)
   end
 end
 
-def plot_tree (commit)
-  if $commits.has_key? commit.id
+def plot_tree (commit, children, boring=[], plotted={})
+  if plotted.has_key? commit.id
     return
   else
-    $commits[commit.id] = 1
-    commit.parents.each do |c|
-      print "\""
-      print commit.message.gsub(%r|\n|, "\\n")
-      puts "\" -> \"#{commit.id.slice 0,7}\" [arrowhead=dot, color= lightgray, arrowtail=vee];"
-      print "\""
-      print commit.message.gsub(%r|\n|, "\\n")
-      puts "\" [shape=box, fontname=courier, fontsize = 8, color=lightgray, fontcolor=lightgray];"
-      puts "\"#{commit.id.slice 0,7}\" -> \"#{c.id.slice 0,7}\";"
-      plot_tree(c)
+    if is_interesting(commit, children)
+      make_node(commit)
+      boring = []
+    else
+      boring += [commit]
     end
+
+    commit.parents.each do |c|
+      if is_interesting(c, children)
+        if is_interesting(commit, children)
+          make_edge(commit, c)
+        else
+          make_elision(boring, c)
+        end
+        boring = []
+      else
+        boring += [c]
+      end
+
+      plot_tree(c, children, boring, plotted)
+    end
+    plotted[commit.id] = true
   end
 end
 
+#      print "\""
+#      print commit.message.gsub(%r|\n|, "\\n")
+#      puts "\" -> \"#{commit.id.slice 0,7}\" [arrowhead=dot, color= lightgray, arrowtail=vee];"
+#      print "\""
+#      print commit.message.gsub(%r|\n|, "\\n")
+#      puts "\" [shape=box, fontname=courier, fontsize = 8, color=lightgray, fontcolor=lightgray];"
+#      puts "\"#{commit.id.slice 0,7}\" -> \"#{c.id.slice 0,7}\";"
+
 def is_interesting(commit, children, neighbor=0)
+  #merge or branch point
   if commit.parents.length > 1
     return true
   end
   if children[commit.id].length > 1
     return true
   end
+
   if neighbor <= 0
     return false
   end
+
+  #parent is interesting
   commit.parents.each do |p|
     if is_interesting(p, children, neighbor - 1)
       return true
     end
   end
+
+  #child is interesting
   children[commit.id].each do |c|
     if is_interesting(c, children, neighbor - 1)
       return true
     end
   end
+
   return false
 end
 
-def show_interesting(commit, children, shown={})
+def nodes_for_interesting(commit, children, shown={})
   if shown.has_key? commit.id
     return
   end
   if is_interesting(commit, children)
-    puts commit.id
+    make_node(commit)
     shown[commit.id] = true
   end
   commit.parents.each do |p|
-    show_interesting(p, children, shown)
+    nodes_for_interesting(p, children, shown)
   end
 end
 
@@ -76,18 +102,42 @@ def plot_tags(repo)
   end
 end
 
+def make_node(commit)
+  puts "\"#{commit.id}\" [label=\"#{commit.id.slice 0,7}\"];"
+end
+
+def make_edge(c1, c2)
+  puts "\"#{c1.id}\" -> \"#{c2.id}\";"
+end
+
+def make_elision(boring_commits, interesting_commit)
+  puts "\"#{boring_commits.first.id}\" [label=\"boring: #{boring_commits.first.id.slice 0,7}\"];"
+  puts "\"#{boring_commits.first.id}\" -> \"#{interesting_commit.id}\";"
+end
+
+repo = Grit::Repo.new(ARGV[0]);
+
+#decorated = (repo.branches + repo.remotes + repo.tags).collect{|r| r.commit}
+decorated = (repo.branches + repo.tags).collect{|r| r.commit}
+d2 = decorated.collect{|c| c.parents}
+
+children = {}
+decorated.each do |c|
+  find_children(c, children)
+end
+
+puts "Digraph Git {"
+plotted={}
+decorated.each do |c|
+  plot_tree(c, children, [], plotted)
+  #nodes_for_interesting(c, children)
+end
+puts "}"
+
 #puts "Digraph F {"
 #puts 'ranksep=0.2;'
-repo = Grit::Repo.new(ARGV[0]);
-children = {}
-repo.branches.each do |b| 
-  find_children(b.commit, children)
-end
-repo.branches.each do |b| 
-  show_interesting(b.commit, children)
   #puts "\"#{b.name}\" -> \"#{b.commit.id.slice 0,7}\";"
   #puts "\"#{b.name}\" [shape=polygon, sides=6, style=filled, color = red];"
   #plot_tree(b.commit)
-end
 #puts "}"  
   
