fad695c4d288bdec1e520d4322196c5ae1645838
Matt McHenry <github@matt.mchenryfamily.org>
2013-01-07 20:58:55 -0500

    make sure commit id is set off from log message

--- git-graph.rb
+++ git-graph.rb
@@ -161,7 +161,7 @@ end
 
 def log_for(commit)
   #escape twice so that xml entities end up correct in svg files
-  wrap_text(commit.message.gsub(%r|git-svn-id: .*$|, "") + id_for(commit)).fast_xs.fast_xs.gsub(%r|\n|, "<br/>")
+  wrap_text(commit.message.gsub(%r|git-svn-id: .*$|, "") + " [" + id_for(commit) + "]").fast_xs.fast_xs.gsub(%r|\n|, "<br/>")
 end
 
 def fixed(str)
