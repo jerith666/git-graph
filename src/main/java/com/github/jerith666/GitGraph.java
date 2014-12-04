9d12328af73e2fa1403cb3fc89afae16f5b24758
Matt McHenry <github@matt.mchenryfamily.org>
2014-12-03 23:13:19 -0500

    intial hacking on getting refs via completable future

--- src/main/java/com/github/jerith666/GitGraph.java
+++ src/main/java/com/github/jerith666/GitGraph.java
@@ -1,5 +1,7 @@
 package com.github.jerith666;
 
+import static org.eclipse.jgit.lib.Constants.*;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,14 +11,20 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 
 import org.eclipse.jgit.errors.IncorrectObjectTypeException;
 import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -31,12 +39,49 @@ import com.google.common.collect.Multimaps;
 import com.google.common.collect.SetMultimap;
 
 public final class GitGraph {
+    @FunctionalInterface
+    private static interface ExceptionalFunction<T,R,E extends Throwable>{
+        R apply(T t) throws E;
+    }
+
+    private static <T,R,E extends Throwable> CompletableFuture<R> applyOrDie(T t, ExceptionalFunction<T,R,E> ef){
+        CompletableFuture<R> cf = new CompletableFuture<R>();
+        try{
+            cf.complete(ef.apply(t));
+        }
+        catch(Throwable e){
+            cf.completeExceptionally(e);
+        }
+        return cf;
+    }
+
+    private static <K,V> BiFunction<Map<? extends K,? extends V>, Map<? extends K,? extends V>, Map<K, V>> mapCollapser(){
+        return (m1, m2) -> {
+            Map<K,V> m = new LinkedHashMap<K,V>(m1);
+            m.putAll(m2);
+            return m;
+         };
+    }
 
     public static void main(String[] args) throws IOException {
         final Repository repo = new FileRepositoryBuilder().findGitDir(new File(args[0]))
                                                            .setMustExist(true)
                                                            .build();
-
+        
+        ExceptionalFunction<String, Map<String, Ref>, IOException> getRefsByPrefix = repo.getRefDatabase()::getRefs;
+        //BiFunction<T, U, R> x = (m1, m2) -> {LinkedHashMap<String, Ref> m = new LinkedHashMap<>(m1); m.putAll(m2); return m;}
+        applyOrDie(R_HEADS, getRefsByPrefix).thenCombine(applyOrDie(R_REMOTES, getRefsByPrefix).thenCombine(applyOrDie(R_TAGS, getRefsByPrefix),
+                                                                                                            mapCollapser()),
+                                                         mapCollapser())
+                                            .thenAccept(refMap -> new LinkedHashMap<>(refMap).entrySet().forEach(System.out::println));
+        System.exit(0);
+//        Stream.of(R_HEADS, R_REMOTES, R_TAGS)
+//              .flatMap(prefix -> repo.getRefDatabase().getRefs(prefix).entrySet().stream());
+        repo.getRefDatabase().getRefs(Constants.R_HEADS);
+        repo.getRefDatabase().getRefs(Constants.R_HEADS);
+        repo.getRefDatabase().getRefs(Constants.R_HEADS);
+        new LinkedHashMap<>(repo.getAllRefs()).entrySet().forEach(System.out::println);
+        
         Set<String> graphSrcCommits;
         if(args.length > 1){
             graphSrcCommits = readStringsFrom(new FileInputStream(new File(args[1])));
