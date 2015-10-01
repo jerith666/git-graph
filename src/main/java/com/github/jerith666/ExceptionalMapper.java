d51c2d5f34389f75534593af611a5e458efdd10f
Matt McHenry <github@matt.mchenryfamily.org>
2015-09-30 22:44:45 -0400

    can't use stock toList() collector because of wildcard

--- src/main/java/com/github/jerith666/ExceptionalMapper.java
+++ src/main/java/com/github/jerith666/ExceptionalMapper.java
@@ -3,19 +3,22 @@ package com.github.jerith666;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public final class ExceptionalMapper {
 
     public static void main(String[] args) {
+        Collector<Class<?>, ?, List<Class<?>>> toList = Collectors.toList();
         CompletableFuture<List<Class<?>>> classes =
                 Stream.of("java.lang.String", "java.lang.Integer", "java.lang.Double")
                       .map(MonadUtils.applyOrDie(Class::forName))
                       .map(cfc -> cfc.thenApply(Class::getSuperclass))
-                      .collect(MonadUtils.cfCollector(ArrayList::new,
-                                                      List::add,
-                                                      (List<Class<?>> l1, List<Class<?>> l2) -> { l1.addAll(l2); return l1; },
-                                                      x -> x));
+                      .collect(MonadUtils.cfCollector(toList.supplier(),
+                                                      toList.accumulator(),
+                                                      toList.combiner(),
+                                                      toList.finisher()));
         classes.thenAccept(System.out::println)
                .exceptionally(t -> { System.out.println("unable to get class: " + t); return null; });
     }
